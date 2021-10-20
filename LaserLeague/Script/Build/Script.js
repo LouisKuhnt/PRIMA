aee3182196b95f30cd4cee0a7e87974b20814f07
Louis Kuhnt <louis.ronald.kuhnt@hs-furtwangen.de>
2021-10-20 13:03:19 +0200

    Commit #13 fixed issues with mtxlocal and added controls

--- LaserLeague/Script/Build/Script.js
+++ LaserLeague/Script/Build/Script.js
@@ -38,24 +38,40 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     document.addEventListener("interactiveViewportStarted", start);
-    let transform;
+    let graph, agentSymbol, laser, agent;
+    let moveLaserBeams;
+    let moveSymbolOfAgent;
+    let deltaTime;
+    const speedLaserRotate = 360;
+    const speedAgentTranslation = 10;
+    const speedAgentRotation = 360;
     function start(_event) {
         viewport = _event.detail;
-        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
-        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-        let graph = viewport.getBranch();
+        graph = viewport.getBranch();
         console.log("graph");
         console.log(graph);
-        console.log("Hier Agent");
-        console.log(graph.getChildrenByName('Agents'));
-        console.log(graph.getChildrenByName('Lasers')[0].getChildrenByName('Center_Node#1')[0]);
-        let laser = graph.getChildrenByName('Lasers')[0].getChildrenByName('Center_Node#1')[0];
-        transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
-        console.log(transform);
+        laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser#1")[0].getChildrenByName("Center")[0];
+        agent = graph.getChildrenByName('Agents')[0].getChildrenByName('Agent#1')[0];
+        agentSymbol = agent.getChildrenByName('Agent_Symbol')[0];
+        moveLaserBeams = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+        moveSymbolOfAgent = agentSymbol.getComponent(ƒ.ComponentTransform).mtxLocal;
+        viewport.camera.mtxPivot.translateZ(-20);
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 60); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
+        deltaTime = ƒ.Loop.timeFrameReal / 1000;
         // ƒ.Physics.world.simulate();  // if physics is included and used
-        transform.rotateZ(6);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]))
+            agent.mtxLocal.translateY(speedAgentTranslation * deltaTime);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]))
+            agent.mtxLocal.translateY(-speedAgentTranslation * deltaTime);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]))
+            agent.mtxLocal.rotateZ(speedAgentRotation * deltaTime);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
+            agent.mtxLocal.rotateZ(-speedAgentRotation * deltaTime);
+        moveLaserBeams.rotateZ(speedLaserRotate * deltaTime);
+        moveSymbolOfAgent.rotateZ(1);
         viewport.draw();
         ƒ.AudioManager.default.update();
     }
--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -5,30 +5,50 @@ namespace Script {
   let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
-  let transform: ƒ.Matrix4x4;
+  let graph, agentSymbol, laser, agent: ƒ.Node;
+  let moveLaserBeams: ƒ.Matrix4x4;
+  let moveSymbolOfAgent: ƒ.Matrix4x4;
+  let deltaTime: number;
+  const speedLaserRotate: number = 360;
+  const speedAgentTranslation: number = 10;
+  const speedAgentRotation: number = 360;
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
-    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-
-    let graph: ƒ.Node = viewport.getBranch();
+    graph = viewport.getBranch();
     console.log("graph");
     console.log(graph);
-    console.log("Hier Agent");
-    console.log(graph.getChildrenByName('Agents'));
 
-    console.log(graph.getChildrenByName('Lasers')[0].getChildrenByName('Center_Node#1')[0]);
+    laser = graph.getChildrenByName("Lasers")[0].getChildrenByName("Laser#1")[0].getChildrenByName("Center")[0];
+    agent = graph.getChildrenByName('Agents')[0].getChildrenByName('Agent#1')[0];
+    agentSymbol = agent.getChildrenByName('Agent_Symbol')[0];
 
-    let laser: ƒ.Node = graph.getChildrenByName('Lasers')[0].getChildrenByName('Center_Node#1')[0];
-    transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
-    console.log(transform);
+    moveLaserBeams = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
+    moveSymbolOfAgent = agentSymbol.getComponent(ƒ.ComponentTransform).mtxLocal;
+
+
+    viewport.camera.mtxPivot.translateZ(-20);
+
+    ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
+    ƒ.Loop.start(ƒ.LOOP_MODE.TIME_REAL, 60);  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
+    deltaTime = ƒ.Loop.timeFrameReal / 1000;
     // ƒ.Physics.world.simulate();  // if physics is included and used
-    transform.rotateZ(6);
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.W, ƒ.KEYBOARD_CODE.ARROW_UP]))
+      agent.mtxLocal.translateY(speedAgentTranslation * deltaTime);
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.S, ƒ.KEYBOARD_CODE.ARROW_DOWN]))
+      agent.mtxLocal.translateY(-speedAgentTranslation * deltaTime);
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.A, ƒ.KEYBOARD_CODE.ARROW_LEFT]))
+      agent.mtxLocal.rotateZ(speedAgentRotation * deltaTime);
+    if(ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.D, ƒ.KEYBOARD_CODE.ARROW_RIGHT]))
+      agent.mtxLocal.rotateZ(-speedAgentRotation * deltaTime);
+
+    moveLaserBeams.rotateZ(speedLaserRotate * deltaTime);
+    moveSymbolOfAgent.rotateZ(1);
+
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
