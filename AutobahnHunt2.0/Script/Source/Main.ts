324048a820f6b93aee9f54482a944ee4658c6b65
Louis Kuhnt <louis.ronald.kuhnt@hs-furtwangen.de>
2023-07-11 14:01:12 +0200

    added camera again

--- AutobahnHunt2.0/Script/Source/Main.ts
+++ AutobahnHunt2.0/Script/Source/Main.ts
@@ -1,11 +1,21 @@
 namespace Script {
   import ƒ = FudgeCore;
+
+  let cameraNode: ƒ.Node;
+  export let graph: ƒ.Node;
+  export let viewport: ƒ.Viewport;
+  export let limit_z: number;
+  export let limit_x: number;
+  let playerModel: ƒ.Node;
+
   ƒ.Debug.info("Main Program Template running!");
 
-  let viewport: ƒ.Viewport;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
+    limit_x = 100;
+    limit_z = 300;
+
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
